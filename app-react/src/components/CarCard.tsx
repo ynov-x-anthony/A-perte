0a949e99fe05003b6535ea4da48d6bddf0eb83e7
Shakil60 <khaldishakil@gmail.com>
2026-09-07 15:30:43 +0200

    Add car images to CarCard component

---
+++ app-react/src/components/CarCard.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+type CarCardProps = {
+  marque: string
+  modele: string
+  annee: number
+  prix: number
+  options: string[]
+  image: string
+}
+
+function CarCard({ marque, modele, annee, prix, options, image }: CarCardProps) {
+  const [favori, setFavori] = useState(false)
+
+  return (
+    <div className={favori ? 'car-card favori' : 'car-card'}>
+      <h3>
+        {marque} {modele}
+      </h3>
+      <img className="car-image" src={image} alt={`${marque} ${modele}`} />
+      <p>Année : {annee}</p>
+      <p className="prix">{prix} €</p>
+      <ul>
+        {options.map((option) => (
+          <li key={option}>{option}</li>
+        ))}
+      </ul>
+      <button onClick={() => setFavori(!favori)}>
+        {favori ? '♥ Dans mes favoris' : '♡ Ajouter aux favoris'}
+      </button>
+    </div>
+  )
+}
+
+export default CarCard
